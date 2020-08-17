2d1a8a723509f9939df33a946df0e0a5e2d63c9b
youyeon <yourain416@gmail.com>
2020-08-17 15:34:41 +0900

    Revise main.js with express framework & body-parser

--- Nodejs/lib/topic.js
+++ Nodejs/lib/topic.js
@@ -1,8 +1,9 @@
+var express = require('express');
+var app = express();
 var template = require('./template.js');
 var db = require('./db');
-var url = require('url');
-var qs = require('querystring');
 var sanitizeHtml = require('sanitize-html'); // npm install -S sanitize-html
+var bodyParser = require('body-parser');
 
 exports.home = function(request, response) {
     db.query(`SELECT * from topic`,function(error,topics) {
@@ -13,18 +14,15 @@ exports.home = function(request, response) {
       `<h2>${title}</h2>${description}`,
       `<a href="/create">create</a>`
       );
-    response.writeHead(200);
-    response.end(html);
+    response.send(html);
   });
 };
 exports.page = function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
     db.query(`SELECT * FROM topic`,function(error,topics) {
         if(error) {
           throw error;
         }
-        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[queryData.id],function(error2,topic) {
+        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[request.params.pageId],function(error2,topic) {
          if(error2) {
            throw error2;
          }
@@ -37,14 +35,13 @@ exports.page = function(request, response) {
            <p>by ${sanitizeHtml(topic[0].name)}</p>
            `,
            `<a href="/create">create</a> 
-             <a href="/update?id=${queryData.id}">update</a>
-             <form action="delete_process" method="post">
-               <input type="hidden" name="id" value="${queryData.id}">
+             <a href="/update/${request.params.pageId}">update</a>
+             <form action="/delete_process" method="post">
+               <input type="hidden" name="id" value="${request.params.pageId}">
                <input type="submit" value="delete">
              </form>`
            );
-         response.writeHead(200);
-         response.end(html);
+         response.send(html);
        })
      });
 }
@@ -74,35 +71,25 @@ exports.create = function(request,response) {
            `,
            `<a href="/create"> create</a>`
          );
-         response.writeHead(200);
-         response.end(html);
+         response.send(html);
          });
      });
 }
 exports.create_process = function(request,response) {
-    var body='';
-    request.on('data',function(data) {
-      body = body + data;
-    });
-    request.on('end',function() {
-      var post = qs.parse(body);
-      db.query(`INSERT INTO topic (title, description,created,author_id) VALUES (?,?,NOW(),?)`, 
-        [post.title,post.description,post.author],
-        function (error,result) {
-          if(error)
-            throw error;
-          response.writeHead(302, {Location : `/?id=${result.insertId}`});
-          response.end();
-        })
-    });
+  var post = request.body;
+  db.query(`INSERT INTO topic (title, description,created,author_id) VALUES (?,?,NOW(),?)`, 
+    [post.title,post.description,post.author],
+    function (error,result) {
+      if(error)
+        throw error;
+      response.redirect(302, `/page/${result.insertId}`);
+    })
 }
 exports.update = function(request,response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
     db.query(`SELECT * FROM topic`,function(error,topics) {
         if (error)
           throw error;
-        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2,topic) {
+        db.query(`SELECT * FROM topic WHERE id=?`, [request.params.pageId], function(error2,topic) {
           if (error2)
             throw error2;
           db.query(`SELECT * FROM author`,function(error, authors) {
@@ -111,55 +98,40 @@ exports.update = function(request,response) {
             var html = template.HTML(sanitizeHtml(topic[0].title), list, 
               `
               <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${sanitizeHtml(topic[0].title)}"></p>
-              <p>
-                <textarea name="description" placeholder="description"> ${sanitizeHtml(topic[0].description)}</textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
+                <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${sanitizeHtml(topic[0].title)}"></p>
+                <p>
+                  <textarea name="description" placeholder="description"> ${sanitizeHtml(topic[0].description)}</textarea>
+                </p>
+                <p>
+                  ${template.authorSelect(authors, topic[0].author_id)}
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
             </form>
             `, 
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update </a>`
+              `<a href="/create">create</a> <a href="/update/${topic[0].id}">update </a>`
               );
-            response.writeHead(200);
-            response.end(html);
+            response.send(html);
           });
         });
       });
 }
 exports.update_process = function(request,response) {
-    var body='';
-   request.on('data',function(data) {
-     body = body + data;
-   });
-   request.on('end',function() {
-     var post = qs.parse(body);
-     db.query(`UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.author, post.id], function (error,result) {
-         if(error)
-           throw error;
-         response.writeHead(302, {Location : `/?id=${post.id}`});
-         response.end();
-       });
-   });
-}
-exports.delete_process = function(request,response) {
-    var body='';
-    request.on('data',function(data) {
-      body = body + data;
-    });
-    request.on('end',function() {
-      var post = qs.parse(body);
-      var id = post.id;
-     db.query(`DELETE FROM topic WHERE id=?`, [id], function (error,result) {
+  var post = request.body;
+  db.query(`UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.author, post.id], function (error,result) {
       if(error)
         throw error;
-      response.writeHead(302, {Location : '/'});
-      response.end();
+      response.redirect(302,`/page/${post.id}`);
     });
-    });
+}
+exports.delete_process = function(request,response) {
+  var post = request.body;
+  var id = post.id;
+  db.query(`DELETE FROM topic WHERE id=?`, [id], function (error,result) {
+  if(error)
+    throw error;
+  response.redirect(302,'/');
+});
 }
--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,42 +1,52 @@
 // npm init : Create package.json
+var express = require('express');
+var app = express();
 var http = require('http');
 var url = require('url');
 var topic = require('./lib/topic');
 var author = require('./lib/author');
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname=url.parse(_url,true).pathname; 
-    if (pathname==='/') {
-      if (queryData.id===undefined) {
-        topic.home(request, response);
-      } else {
-        topic.page(request, response);
-    }
-  } else if (pathname==='/create') {
-    topic.create(request,response);
-  } else if (pathname==='/create_process') {
-    topic.create_process(request,response);
-  } else if (pathname==='/update') {
-    topic.update(request,response);
-  } else if (pathname==='/update_process') {
-    topic.update_process(request,response);
-  } else if (pathname==='/delete_process') {
-    topic.delete_process(request,response);
-  } else if (pathname==='/author') {
-    author.home(request,response);
-  } else if (pathname==='/author/create_process') {
-    author.create_process(request,response);
-  } else if (pathname==='/author/update') {
-    author.update(request,response);
-  } else if (pathname==='/author/update_process') {
-    author.update_process(request,response);
-  } else if (pathname==='/author/delete_process') {
-    author.delete_process(request,response);
-  } else {
-    response.writeHead(404);
-    response.end('Not Found')
-  }
-});
-app.listen(3000); //Starts the HTTP server listening for connections (Parameter: PortNum)
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(request, response) {
+  topic.home(request, response);
+});
+app.get('/page/:pageId', function(request, response) {
+  topic.page(request, response);
+});
+app.get('/create', function(request, response) {
+  topic.create(request,response);
+});
+app.post('/create_process', function(request, response) {
+  topic.create_process(request, response);
+});
+app.get('/update/:pageId', function(request, response) {
+  topic.update(request, response);
+});
+app.post('/update_process', function(request, response) {
+  topic.update_process(request, response);
+});
+app.post('/delete_process', function(request, response) {
+  topic.delete_process(request, response);
+});
+app.get('/author', function(request, response) {
+  author.home(request, response);
+});
+app.post('/author/create_process', function(request, response) {
+  author.create_process(request, response);
+});
+app.get('/author/update/:authorId', function(request, response) {
+  author.update(request, response);
+});
+app.post('/author/update_process', function(request, response) {
+  author.update_process(request, response);
+});
+app.post('/author/delete_process', function(request, response) {
+  author.delete_process(request, response);
+});
+app.listen(3000,function(){});
+
+// app.use(function (req, res, next) {
+//   res.status(404).send('404 NOT FOUND');
+// });
