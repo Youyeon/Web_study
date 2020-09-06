676b26cdc8eedfb5a9686a9b85b6bddea386685a
youyeon <yourain416@gmail.com>
2020-09-06 15:08:15 +0900

    cookie

--- cookie_session/cookie.js
+++ cookie_session/cookie.js
@@ -6,7 +6,14 @@ http.createServer(function(request, response) {
         var cookies = cookie.parse(request.headers.cookie);
     }
     response.writeHead(200, {
-        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    });
+        'Set-Cookie':['yummy_cookie=choco', 
+        'tasty_cookie=strawberry',
+        `Permanent=cookies; Max-Age=${60*60*24*30}`, //cookie expired time set, unit=second
+        'Secure=Secure; Secure', //뒤의 Secure: 웹이 https로 통신할 때만 쿠키 생성
+        'HttpOnly=HttpOnly; HttpOnly', //JavaScript를 이용한 침해 방지
+        'Path=Path; Path=/cookie', //cookie path 
+        'Domain=Domain; Domain=o2.org' //cookie domain: 별도 설정 필요
+        ]
+    }); 
     response.end('Cookie!');
 }).listen(3000);
